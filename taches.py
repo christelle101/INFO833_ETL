9db5752d4a34e1f650ba7f7a0121e4d4f0228c1f
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2022-05-02 09:43:26 +0200

    05/02/2022 version

---
+++ taches.py
@@ -0,0 +1,66 @@
+class T1:
+
+    def __init__(params):
+        """
+        params is a dictionary that contains all variables and parameters needed to execute the task.
+        In particular, params contains all input values to the task.
+        """
+        pass
+
+    def extract():
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
+
+class T2:
+
+    def __init__(params):
+        """
+        params is a dictionary that contains all variables and parameters needed to execute the task.
+        In particular, params contains all input values to the task.
+        """
+        pass
+    def extract():
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
+
+class T3:
+
+    def __init__(params):
+        """
+        params is a dictionary that contains all variables and parameters needed to execute the task.
+        In particular, params contains all input values to the task.
+        """
+        pass
+
+    def extract():
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
+        
+class Taches:
+
+    def __init__(self):
+        pass
+    
+    def T1(params):
+        return T1(params)
+    
+    def T2(params):
+        return T2(params)
+
+    def T3(params):
+        return T3(params)
