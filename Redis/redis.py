f2ca9814df9839877820184a33dbb0d2fcf36949
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2022-05-10 19:45:55 +0200

    Update redis.py

--- Redis/redis.py
+++ Redis/redis.py
@@ -1,4 +1,32 @@
-import redis, taches, string
+import redis, taches, string, random
 
 r = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8")
 r.delete("task_queue")
+
+def random_hash():
+    return "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+
+params = {"path" : "dataT1.json"}
+hash = random_hash()
+r.hset(hash, mapping = params)
+r.rpush("task_queue", f"T1 : {hash}")
+
+while (r.llen("task_queue") > 0):
+    task = (r.lpop("task_queue")).decode("utf-8")
+    taskName = task.split(":")[0]
+    hash = task.split(":")[1]
+    params = r.hgetall(hash)
+    r.delete(hash)
+    params = {k.decode("utf-8"): v.decode("utf-8") for k, v in params.items()}
+    
+    taskObject = getattr(taches, taskName)(params)
+    taskObject.extract()
+    data = taskObject.load()
+    
+    if ("task" in data):
+        hash = random_hash()
+        params = data["params"] or {}
+        r.hset(hash, mapping=params)
+        r.rpush("task_queue", data["task"] + ":" + hash)
+    else:
+        print(data)
