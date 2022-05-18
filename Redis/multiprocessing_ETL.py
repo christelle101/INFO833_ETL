7f67164dd2c5d0080fb50f129b2d0de4efc57499
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2022-05-18 22:20:20 +0200

    Create multiprocessing_ETL.py

---
+++ Redis/multiprocessing_ETL.py
@@ -0,0 +1,7 @@
+from multiprocessing import Process
+import tasks, redis, time, string, random
+
+r = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8")
+
+def random_hash():
+    return "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
